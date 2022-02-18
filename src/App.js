be7a5ca9cdab93152057516ced3f9bcc8e5a54aa
Lucca Rendall <luccarendall1@gmail.com>
2022-02-18 18:11:27 -0300

    Erro na criaçao do componente - Req 1

--- src/App.js
+++ src/App.js
@@ -7,3 +7,5 @@ class App extends React.Component {
 }
 
 export default App;
+
+// Exemplo: <button data-testid="my-action"></button>
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    };
+     <header>
+         <h1>Sistema Solar</h1>
+     </header>
+    ;
+  };
+
+export default Header;
