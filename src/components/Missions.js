a69cb4b74daf2c9722227a866744825932f63603
Lucca Rendall <luccarendall1@gmail.com>
2022-02-19 05:10:42 -0300

    Inserindo prop headline com o valor Missões no componente Title

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Title from './Title';
-
+// O componente Title deve ser renderizado recebendo a prop headline com o valor "Missões"
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <>
+        <div data-testid="missions" />
+        <Title headline="Missões" />
+      </>
     );
   }
 }
