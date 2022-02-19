16408ba8a24b3f4a83b0ad06714dca9785657627
Lucca Rendall <luccarendall1@gmail.com>
2022-02-19 04:37:49 -0300

    Criando componente Missions

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -8,7 +8,7 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title key="headline" headline="Planetas" />
-        {Planets.map(
+        {Planets.map( // método que te permite criar vários componentes iguais a partir dos valores presentes em um array
           (event, i) => (<PlanetCard
             key={ i }
             planetName={ event.name }
